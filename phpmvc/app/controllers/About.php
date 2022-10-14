c2c5ff4050f127e6f507b47e20371bffc5a9726d
Andip29 <priyandi.zembarazizi@gmail.com>
2022-10-14 09:21:38 +0700

    menyelesaikan video pembelajaran 5

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,12 +1,21 @@
-<?php 
+<?php
 
-class About
-{
-  public function index($nama = 'Priyandi', $pekerjaan = 'Mahasiswa'){
-    echo "Hallo, nama saya $nama, saya adalah seorang $pekerjaab";
-  }
-  public function page(){
-    echo 'About/page';
-  }
-  
+class About extends Controller {
+    public function index($nama= 'Priyandi', $pekerjaan = 'Mahasiswa', $umur = 21)
+    {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+    }
+    public function page()
+    {
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
+    }
 }
