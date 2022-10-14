cf039e7d95e03f14b013aeeb012ac69a2d0a167a
Andip29 <priyandi.zembarazizi@gmail.com>
2022-10-14 10:32:27 +0700

    menyelesaikan video pembelajaran 7 Assets

--- phpmvc/app/views/home/index.php
+++ phpmvc/app/views/home/index.php
@@ -1,2 +1,9 @@
-
-    <h1>Selamat datang di website saya</h1>
+<div class="container">
+<div class="jumbotron mt-4">
+        <h1 class="display-4">Selamat Datang di Website Saya</h1>
+        <p class="lead">This is a simple hero unit, a somple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr class="my-4">
+        <p>It uses utility classes for tygoraphy and spacing to space content out within the larger container.</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">learn more</a>
+    </div>
+</div>
